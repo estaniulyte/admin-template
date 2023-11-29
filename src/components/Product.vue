0984b10c105954db57d7bce274f0ffebff9dd392
estaniulyte <43463977+estaniulyte@users.noreply.github.com>
2023-11-29 11:51:17 +0200

    change: re-design products page

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -5,6 +5,7 @@ const route = useRoute();
 const id = route.params.id as string;
 
 import { useProduct } from '../utils/useProducts';
+import Rating from './Rating.vue';
 
 const { data: product, isLoading, error } = useProduct(parseInt(id));
 </script>
@@ -19,10 +20,22 @@ const { data: product, isLoading, error } = useProduct(parseInt(id));
         <span class="capitalize">{{ product?.category }}</span>
       </p>
       <h1 class="mt-3 text-4xl uppercase">{{ product?.title }}</h1>
-      <h2 class="text-xl">{{ product?.brand }}</h2>
-      <span>${{ product?.price }}</span>
-      <p>{{ product?.stock }} items in stock</p>
-      <p>{{ product?.rating }} rating</p>
+      <div class="flex items-center gap-x-4 text-sm font-medium text-gray-500">
+        <Rating class="my-3" :rating="product?.rating!" />
+        |
+        <p class="">
+          <span class="font-bold text-gray-600">{{ product?.stock }}</span>
+          items in stock
+        </p>
+        |
+        <p>
+          Brand:
+          <span class="uppercase font-bold text-gray-600">{{
+            product?.brand
+          }}</span>
+        </p>
+      </div>
+      <h3 class="text-3xl my-3">${{ product?.price }}</h3>
     </div>
   </div>
 </template>
